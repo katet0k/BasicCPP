2682b9d7597435c4580b2bf7cb346ffcf3f23925
Катя <@DESKTOP-VT3CEPG>
2022-10-03 12:04:44 +0300

    15.09.22

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,7 +1,71 @@
 #include <iostream>
 using namespace std;
 
-int maun()
+int main()
 {
+	cout << "\t" << endl;
+
+	cout << "\t  Name\t         :\t\"The war and the peace\"" << endl
+		<< "\t  Avtor \t :\t\ L.N.Tolstoj" << endl
+		<< "\t  Izdatelstvo\t :\t\ Piter" << endl
+		<< "\t  Pages \t : \t\ 500." << endl;
+
+	cout << "\t" << endl;
+	cout << "\t" << endl;
+
+
+	cout << "\t  \"Kraj lukomor’a dub zelenyj," << endl
+		<< "\t  I zolotyj lancyuh na nim :" << endl
+		<< "\t  Shodnya, shonochi kit uchenyj" << endl
+		<< "\t  Na lancyuhu kruzhl’aje tim\"" << endl;
+
+
+	cout << "\n         " << (char)201 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)187
+		<< endl
+
+		<< "\t " << (char)186 << "\t                           Pory Roku \t                            " << (char)186 << endl
+
+		<< "\t " << (char)204 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)203
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)203
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)203
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)185
+		<< endl
+
+		<< "\t " << (char)186
+		<< "\t Zyma\t    " << (char)186
+		<< "\t  Vesna\t       " << (char)186
+		<< "\tLito \t " << (char)186
+		<< "\t Osin\t    " << (char)186
+		<< endl
+
+		<< "\t " << (char)200 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)202
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)202
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)202
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205 << (char)205
+		<< (char)188 << endl << "\t   " << endl << "\t ";
+
+
 	return 0;
 }
