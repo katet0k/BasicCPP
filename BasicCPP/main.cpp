d107aeea8201859b52d7bb88cff74a5676ac7b00
Катя <@DESKTOP-VT3CEPG>
2022-10-16 21:25:30 +0300

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -3,7 +3,17 @@
 
 using namespace std;
 
-
+void RandKub(int N, int K)
+{
+    for (size_t i = 0; i <= N; i++)
+    {
+        for (size_t j = 0; j <= K; j++)
+        {
+            cout << " * " << " ";
+        }
+        cout << endl;
+    }
+}
 void RandMatrix(int arr[][5], const int size)
 {
     for (size_t i = 0; i < size; i++)
@@ -42,7 +52,37 @@ void Print(int arr[], const int size)
     cout << endl;
 }
 
+bool Prost(int N)
+{
+    uint32_t counter = 0;
+    for (size_t i = 1; i <= N; i++)
+    {
+        if (N % i == 0)
+            counter++;
+        if (counter >= 3)
+            return false;
+    }
+    return true;
+}
+
+void kub(int N)
+{
+    int copy = N;
+    for (int i = 1; i < 3; i++)
+    {
+        N *= copy;
+
+    }
+
+    cout << N;
+}
+
+
 int main() {
+
+
+    setlocale(LC_ALL, "ru");
+
     int num;
     cin >> num;
 
@@ -50,38 +90,15 @@ int main() {
     {
     case 1:
     {
-        const int size = 5;
-        int arr[size][size][size][size];
-        for (size_t i = 0; i < size; i++)
-        {
-            for (size_t j = 0; j < size; j++)
-            {
-                for (size_t k = 0; k < size; k++)
-                {
-                    for (size_t x = 0; x < size; x++)
-                    {
-                        arr[i][j][k][x] = rand() % 10;
-                    }
-                }
-            }
-        }
-        for (size_t i = 0; i < size; i++)
-        {
-            for (size_t j = 0; j < size; j++)
-            {
-                for (size_t k = 0; k < size; k++)
-                {
-                    for (size_t x = 0; x < size; x++)
-                    {
-                        cout << arr[i][j][k][x] << " ";
-                    }
-                    cout << "\t";
-                }
-                cout << endl;
-            }
-            cout << endl << endl;
-        }
+        int N, K;
+        cout << " введите число ";
+        cin >> N;
+        cout << " введите число ";
+        cin >> K;
 
+        RandKub(N, K);
+
+        system("pause");
 
     }break;
 
@@ -111,8 +128,27 @@ int main() {
 
     case 3:
     {
+        int N;
+        int c;
+        cin >> N;
+        if (Prost(N)) {
+            cout << "This number is simple!" << endl;
+        }
+        else
+        {
+            cout << "This number is not simple!" << endl;
+        }
 
-    }
+    }break;
+
+    case 4:
+    {
+        int N = 4;
+        cin >> N;
+
+        kub(N);
+
+    }break;
 
     default:
         break;
