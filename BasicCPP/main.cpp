a9b014b8655999867ec63cb4944e28e683db9680
Катя <@DESKTOP-VT3CEPG>
2022-10-28 23:00:50 +0300

    gotovo

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,109 +1,486 @@
 #include <iostream>
+#include <Windows.h>
+#include <ctime>
+#include<conio.h>
 using namespace std;
 
 
+/////1
+bool Year(int year)
+{
+	bool res = 0;
 
-void numbr1(int d1, int m1, int ye1, int d2, int m2, int ye2, int k) {
+	if (year % 4 == 0) {
+		res = true;
+	}
+	if (year % 100 == 0) {
+		res = false;
+	}
+	if (year % 400 == 0) {
+		res = true;
+	}
+	return res;
+}
+int Date(int d, int m, int y)
+{
 
+	int k = d;
 
-	k = d1;
-
-	if (m1 == 2 || m2 == 2) {
-		if (ye1 || ye2 % 4 == 0) {
+	if (m == 1)
+	{
+		k += 31;
+	}
+	if (m == 2)
+	{
+		if (Year(y)) {
 			k += 29;
 		}
 		else {
 			k += 28;
 		}
-		
 	}
-	if (m1 > 12 || m2 > 12)
+	if (m == 3)
+	{
+		k += 31;
+	}
+	if (m == 4)
 	{
 
-		if (d1 == 1 || d2 == 1) {
-			k += 31;
-		}
-		if (d1 == 2 || d2 == 2) {
-			k += 30;
-		}
-		if (d1 || d2 == 3) {
-			k += 31;
-		}
-		if (d1 || d2 == 4) {
-			k += 30;
-		}
-		if (d1 || d2 == 5) {
-			k += 31;
-		}
-		if (d1 || d2 == 6) {
-			k += 30;
-		}
-		if (d1 || d2 == 7) {
-			k += 31;
-		}
-		if (d1 || d2 == 8) {
-			k += 31;
-		}
-		if (d1 || d2 == 9) {
-			k += 30;
-		}
-		if (d1 || d2 == 10) {
-			k += 31;
-		}
-		if (d1 || d2 == 11) {
-			k += 30;
-		}
-		if (d1 || d2 == 12) {
-			k += 31;
-		}
 	}
-	if (d1 > 31) {
-		m1 += 1;
+	if (m == 5)
+	{
+		k += 31;
 	}
-	if (d2 > 31) {
-		m2 += 1;
+	if (m == 6)
+	{
+
+	}
+	if (m == 7)
+	{
+		k += 31;
+	}
+	if (m == 8)
+	{
+		k += 31;
+	}
+	if (m == 9)
+	{
+
+	}
+	if (m == 10)
+	{
+		k += 31;
+	}
+	if (m == 11)
+	{
+
+	}
+	if (m == 12)
+	{
+		k += 31;
 	}
 
+	k += (y - 1) * 365 + ((y - 1) / 4);
+
+	return k;
+}
+int Difference(int d1, int d2, int m1, int m2, int y1, int y2)
+{
+	if (d1 > d2) {
+		int temp = d1;
+		d1 = d2;
+		d1 = temp;
+	}
+	if (m1 > m2) {
+		int temp = m1;
+		m1 = m2;
+		m2 = temp;
+	}
+	if (y1 > y2)
+	{
+		int temp = y1;
+		y1 = y2;
+		y2 = temp;
+	}
+
+	if (d2 > d1) {
+		int temp = d2;
+		d2 = d1;
+		d1 = temp;
+	}
+	if (m2 > m1) {
+		int temp = m2;
+		m2 = m1;
+		m1 = temp;
+	}
+	if (y2 > y1)
+	{
+		int temp = y2;
+		y2 = y1;
+		y1 = temp;
+	}
+	int k = Date(d1, m1, y1) - Date(d2, m2, y2);
+	return k;
+}
+
+
+/////2
+void Arifmet( int arr[], const int size, int n = 0 ,int sum = 0)
+{
+
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+	cout << " ";
+	for (n = 0; size > n; n++)
+	{
+		arr[n] = rand() % 10;
+		SetConsoleTextAttribute(handle, WORD(0 << 0 | 10));
+		cout << arr[n] << "\t ";
+		SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+		sum += arr[n] % 10;
+
+	}
+	cout << endl;
+	cout << " \n —реднее арифметическое = ";
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << (double)sum / n << endl;
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
 
 }
 
+////3
+void KolElement( int arr[], const int size, int n = 0, int c = 0) {
+
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	int plus = 0;
+	int minys = 0;
+	cout << " ";
+	for (size_t i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20 + (-5);
+		SetConsoleTextAttribute(handle, WORD(0 << 0 | 10));
+		cout << arr[i] << "\t ";
+
+		if (arr[i] > 0)
+		{
+			plus++;
+		}
+	    else if (arr[i] < 0) {
+			minys++;
+		}
+		if (arr[i] % 10 == 0) {
+			c++;
+		}
+		arr[i] /= 10;
+	}
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+	cout << endl;
+	cout << " \n  оличество нулей = ";
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << c << endl;
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+	cout << " \n  оличество положительных чисел = ";
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << plus << endl;
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+	cout << " \n  оличество отрицательных чисел = ";
+
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << minys << endl;
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+}
+/////4
+
+void Arr(int arr[][5], const size_t col, const size_t row) {
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	for (size_t i = 0; i < col; i++)
+	{
+		cout << " ";
+		for (size_t j = 0; j < row; j++)
+		{
+			arr[col][row] = arr[i][j];
+
+			arr[i][j] = rand() % 20 + (-5);
+			
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 10));
+			cout << arr[i][j] << "\t\t";
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+		}
+		cout << endl << endl;
+
+	}
+	
+
+	
+	
+}
+void MinMax(int arr[][5], const int col, int row)
+{
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	long long min, max;
+	max = arr[col][row];
+	min = arr[col][row];
+
+	for (size_t i = 0; i < col; i++)
+	{
+		for (size_t j = 0; j < row; j++)
+		{
+			if (min > arr[i][j])
+			{
+				min = arr[i][j];
+			}
+
+			if (max < arr[i][j])
+			{
+				max = arr[i][j];
+			}
+		}
+	}
+
+	cout << " Min = ";
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << min << endl;
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+	cout << " Max = ";
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+	cout << max << endl;
+	SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+}
+
+/*
+void Sort(int arr[][5], const size_t col, const size_t row) {
+
+	for (size_t i = 1; i < row; i++) {
+
+		for (size_t j = i; j > 0; j--) {
+
+			if (arr[j] < arr[j - 1])
+			{
+				arr[i][j] = arr[col][row];
+				int swap = arr[i][j];
+				arr[i][j] = arr[i][j - 1];
+				arr[i][j - 1] = swap;
+
+			}
+		}
+
+	}
+}
+*/
 
 
 int main()
 {
+
+	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
 	setlocale(LC_ALL, "ru");
 
-	int num;
-	cin >> num;
+	char switch_on;
 
-	switch (num) {
-	case 1:
+	do
 	{
 
-		int d1, d2, m1, m2, ye1, ye2;
-		int k = 0;
-		cout << " введите день ";
-		cin >> d1;
-		cout << " введите мес€ц ";
-		cin >> m1;
-		cout << " введите год ";
-		cin >> ye1;
+		system("cls");
+		cout << "    #=====================MENU====================#" << endl;
+		cout << "     |                                           | " << endl;
+		cout << "     |   1 - вычисл€ет разницу в дн€х между      |" << endl;
+		cout << "     |     введенными датами                     |" << endl;
+		cout << "     |                                           |" << endl;
+		cout << "     |   2 - определ€ет среднее арифметических   |" << endl;
+		cout << "     |     элементов переданного ей массива.     |" << endl;
+		cout << "     |                                           |" << endl;
+		cout << "     |   3 - определ€ет количество положительных,|" << endl;
+		cout << "     |   отрицательных и нулевых элементов       |" << endl;
+		cout << "     |   переданного ей массива.                 |" << endl;
+		cout << "     |                                           |" << endl;
+		cout << "     |   4 - перегруженные функции (ну почти)    |" << endl;
+		cout << "     |                                           |" << endl;
+		cout << "     |   0 - выход                               |" << endl;
+		cout << "     |                                           | " << endl;
+		cout << "    #============================================#" << endl;
 
-		//cout << " введите день ";
-		//cin >> d2;
-		//cout << " введите мес€ц ";
-		//cin >> m2;
-		//cout << " введите год ";
-		//cin >> ye2;
+		cout << "\n ¬ведите число - > ";
+		switch_on = _getch();
+		system("cls");
+		cout << endl;
+		switch (switch_on) {
 
-		numbr1(d1, d2, m1, m2, ye1, ye2,k);
 
-		cout << d1 << "_" << m1 << "_" << ye1 << endl;
-	}
+		case '0':
+		{
+			break;
+		}break;
+		case '1':
+		{
 
-	default:
-		break;
-	}
+			int d1, d2, m1, m2, y1, y2;
+
+
+
+			cout << " ¬ведите первый день: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> d2;
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+			cout << " ¬ведите первый мес€ц: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> m2;
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+			for (size_t i = 0; i < m2; i++)
+			{
+				if (m2 > 12 || m2 < 1)
+				{
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 4));
+					cout << " ќшибка введите число от 1 до 12" << endl;
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+					cout << " ¬ведите первый мес€ц: ";
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+					cin >> m2;
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+				}
+			}
+
+			cout << " ¬ведите первый год: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> y2;
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+			cout << " ¬ведите второй день: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> d1;
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+			cout << " ¬ведите второй мес€ц: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> m1;
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+			for (size_t i = 0; i < m1; i++)
+			{
+				if (m1 > 12 || m1 < 1)
+				{
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 4));
+					cout << " ќшибка введите число от 1 до 12" << endl;
+
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+					cout << " ¬ведите второй мес€ц: ";
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+					cin >> m1;
+
+					SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+				}
+			}
+
+			cout << " ¬ведите второй год: ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 2));
+			cin >> y1;
+
+
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+			cout << " \n –азность в дн€х между этими датами составл€ет ";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 10));
+			cout << Difference(d1, d2, m1, m2, y1, y2) << " дней.\n";
+
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+			cout << endl;
+			cout << " ";
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 8));
+			system("pause");
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+		}break;
+
+		case '2':
+		{
+
+			srand(time(0));
+
+			int n = 0, sum = 0;
+			const int size = 10;
+			int arr[size];
+
+			Arifmet(arr, size, n, sum);
+
+			cout << endl;
+			cout << " ";
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 8));
+			system("pause");
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+		}break;
+
+		case '3':
+		{
+
+			srand(time(0));
+
+			int n = 0, sum = 0, c = 0;
+			const int size = 10;
+			int arr[size];
+
+			KolElement(arr, size, n = 0, c = 0);
+
+			cout << endl;
+			cout << " ";
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 8));
+			system("pause");
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+
+		}break;
+
+		case '4':
+		{
+			srand(time(0));
+			const size_t col = 5;
+			const size_t row = 5;
+			int arr[col][row];
+
+			/*Sort(arr, col, row);*/
+			Arr(arr,col, row);
+			MinMax(arr, col, row);
+
+			cout << endl;
+			cout << " ";
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 8));
+			system("pause");
+			SetConsoleTextAttribute(handle, WORD(0 << 0 | 7));
+		}break;
+
+		default:
+			break;
+		}
+
+	} while (switch_on != '0');
 
 	return 0;
 }
