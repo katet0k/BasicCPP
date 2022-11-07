4a18657ef9223b7d831d3aae5614619ea931a18f
Катя <@DESKTOP-VT3CEPG>
2022-11-07 19:22:59 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <conio.h>
+#include <Windows.h>
 using namespace std;
 
 int main()
 {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+
+    setlocale(LC_ALL, "ru");
+    char switch_on;
+    do
+    {
+        system("cls");
+        cout << "    #=====================MENU====================#" << endl;
+        cout << "     |                                           | " << endl;
+        cout << "     |                                           | " << endl;
+        cout << "    #==============================================#" << endl;
+        cout << "     |   0 - Выход                               |" << endl;
+        cout << "    #==============================================#" << endl;
+
+        cout << "\n Введите число - > ";
+        switch_on = _getch();
+        system("cls");
+        cout << endl;
+
+        switch (switch_on)
+        {
+        case '0': {
+            break;
+        }break;
+
+
+        default:
+            break;
+        }
+    
+    } while (switch_on != '0');
+
 	return 0;
 }
