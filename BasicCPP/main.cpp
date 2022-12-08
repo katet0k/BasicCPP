b5359d19ad4660d17f43c0f6661eed2a8a804ccb
Катя <@DESKTOP-VT3CEPG>
2022-12-08 20:51:10 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
-#include <conio.h>
-#include <Windows.h>
+#include <algorithm>
 using namespace std;
-
+struct Point //Entity
+{
+    int x;
+    int y;
+};
+void update_point(Point* point)
+{
+    cout << "Enter x: ";
+    cin >> point->x;
+    cout << "Enter y: ";
+    cin >> point->y;
+}
+void show_point(Point* point)
+{
+    cout << "x = " << point->x << endl;
+    cout << "y = " << point->y << endl;
+}
+struct ShowPoint //Use Cases
+{
+    Point* point = nullptr;
+    void (*action)(Point*);
+};
 int main()
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
-
-    setlocale(LC_ALL, "ru");
-    char switch_on;
-    do
-    {
-        system("cls");
-        cout << "    #=====================MENU====================#" << endl;
-        cout << "     |                                           | " << endl;
-        cout << "     |                                           | " << endl;
-        cout << "    #=============================================#" << endl;
-        cout << "     |   0 - Exit                                |" << endl;
-        cout << "    #=============================================#" << endl;
-
-        cout << "\n --> ";
-        switch_on = _getch();
-        system("cls");
-        cout << endl;
-
-        switch (switch_on)
-        {
-        case '0': {
-            break;
-        }break;
-
-
-        default:
-            break;
-        }
-    
-    } while (switch_on != '0');
-
-	return 0;
+    Point* poin1 = new Point();
+    poin1->x = 3;
+    poin1->y = 6;
+    Point* poin2 = new Point();
+    poin2->x = 2;
+    poin2->y = 3;
+    ShowPoint* showPoint = new ShowPoint;
+    /*showPoint->point = poin1;    
+    showPoint->action = show_point; 
+    showPoint->action(showPoint->point);*/ 
+    showPoint->point = poin2;
+    showPoint->action = show_point;
+    showPoint->action(showPoint->point);
+    showPoint->action = update_point;
+    showPoint->action(showPoint->point);
+    showPoint->action = show_point;
+    showPoint->action(showPoint->point);
+    return 0;
 }
