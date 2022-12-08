8c4131e8c3238d0b8c58288651275585c1270b9e
Катя <@DESKTOP-VT3CEPG>
2022-12-08 20:17:28 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -2,6 +2,74 @@
 #include <conio.h>
 #include <Windows.h>
 using namespace std;
+int mystrlen(const char* str)
+{
+    int size = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        size++;
+    }
+    return size;
+}
+char* mystrcpy(char* str1, const char* str2)
+{
+    int size = mystrlen(str2);
+    str1 = new char[size + 1];
+    for (size_t i = 0; i < size; i++)
+    {
+        str1[i] = str2[i];
+    }
+    str1[size] = '\0';
+    return str1;
+}
+char* mystrcat(char* str1, const char* str2) {
+
+    int size1 = mystrlen(str1);
+    int size2 = mystrlen(str2);
+    char* buf = new char[size1 + size2 + 1];
+    int k = 0;
+    for (size_t i = 0; i < size1; i++,k++)
+    {
+        buf[k] = str1[i];
+    }
+    for (size_t i = 0; i < size2; i++,k++)
+    {
+        buf[k] = str2[i];
+
+    }
+
+    buf [size1 + size2] = '\0';
+    str1 = buf;
+    return str1;
+}
+char* mystrchr(char* str, char s) {
+    int size = mystrlen(str);
+    for (size_t i = 0; i < size; i++)
+    {
+        if (str[i] == s) {
+            return (str + i);
+        }
+    }
+    return 0;
+}
+char* mystrstr(char* str1, char* str2) {
+    int size1 = mystrlen(str1);
+    int size2 = mystrlen(str2);
+
+    for (size_t i = 0; i < size1; i++)
+    {
+
+        if (str1[i] = str2[i]) {
+
+            for (size_t j = 0; j < size2; j++)
+            {
+
+            }
+        }
+    }
+
+    return 0;
+}
 
 int main()
 {
@@ -31,12 +99,62 @@ int main()
             break;
         }break;
 
+        case '1':
+        {
+            char str[] = "Hello";
+            cout << str << endl;
+            cout << "str = " << mystrlen(str) << endl;
+            system("pause");
 
+        }break;
+        case '2':
+        {
+            char str1[] = "Hello!";
+            char str2[] = "World!";
+            cout << "str1 --> " << str1 << endl;
+            cout << "str2 --> " << str2 << endl;
+            cout << "str1 --> " << mystrcpy(str1, str2) << endl;
+            cout << "str2 --> " << str2 << endl;
+            system("pause");
+
+        }break;
+
+        case '3':
+        {
+            char str1[] = "Hello!";
+            char str2[] = "World!";
+            cout << "str1 --> " << str1 << endl;
+            cout << "str2 --> " << str2 << endl;
+            cout << "str1 --> " << mystrcat(str1, str2) << endl;
+            system("pause");
+        }break;
+
+        case '4':
+        {
+            char* str = new char[7] {"Hello"};
+            char s;
+            cin >> s;
+            char* sign = mystrchr(str, s);
+
+            for (size_t i = 0; i < mystrlen(str); i++)
+            {
+                cout << *(str + i) << " --> " << (long long)(str + i) << endl;
+            }
+
+            cout << (long long)sign << endl;
+            system("pause");
+
+        }break;
+
+        case '5':
+        {
+
+        }break;
         default:
             break;
         }
-    
+
     } while (switch_on != '0');
 
-	return 0;
+    return 0;
 }
