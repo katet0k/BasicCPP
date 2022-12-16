668a3b21f5101a0a203dd823f69083454271fe15
Катя <@DESKTOP-VT3CEPG>
2022-12-16 21:23:52 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
@@ -12,6 +13,8 @@ int main()
     char switch_on;
     do
     {
+        
+
         system("cls");
         cout << "    #=====================MENU====================#" << endl;
         cout << "     |                                           | " << endl;
@@ -31,6 +34,28 @@ int main()
             break;
         }break;
 
+        case '1':
+        {
+            FILE* file1;
+            FILE* file2;
+            char str1[256];
+            char str2[256];
+            char path[] = "file1.txt";
+            char path[] = "file2.txt";
+
+            string text, _mode;
+            if ((file1 = fopen(path, _mode.c_str())) == NULL)
+            {
+                perror("Error occured while opening.txt");
+            }
+            while (fgetc(file1)) {
+                
+
+
+            }
+
+
+        }break;
 
         default:
             break;
