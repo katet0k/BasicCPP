6ca68241cbf97d440338b49db12cbc5329840f69
Катя <@DESKTOP-VT3CEPG>
2023-01-12 18:09:51 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,14 +1,79 @@
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
+#include <string>
+#include <sstream>
 using namespace std;
+int mystrlen(const char* str)
+{
+    int size = 0;
+    for (size_t i = 0; str[i] != '\0'; i++)
+    {
+        size++;
+    }
+    return size;
+}
+int StringToNumber(char* str) {
+    cout << (int)str;
+    return 0;
+}
 
+char* NumberToString(int number) {
+    char str1[256] = {"я не док≥нц€ зрозум≥ла €к це делать, ну допустим "};
+    cout << number;
+    return str1;
+}
+char* Uppercase(char* str1) {
+    char A = 65;
+    char a = 97;
+    for (size_t i = 0; i < 25; i++)
+    {
+        for (size_t i = 0; i < str1[i]; i++)
+        {
+            if (str1[i] == a) {
+                str1[i] = A;
+            }
+        }
+        a++;
+        A++;
+    }
+    cout << "—“–ќ ј ” ¬≈–’Ќ№ќћ” –≈√≤—“–≤ --> ";
+    return str1;
+}
+char* Lowercase(char* str1) {
+    char A = 65;
+    char a = 97;
+    for (size_t i = 0; i < 25; i++)
+    {
+        for (size_t i = 0; i < str1[i]; i++)
+        {
+            if (str1[i] == A) {
+                str1[i] = a;
+            }
+        }
+        a++;
+        A++;
+    }
+    cout << "строка у нижньому рег≥стр≥ --> ";
+    return str1;
+}
+char* mystrrev(char* str) {
+
+    for (int i = 4; i != -1; i--)
+    {
+        cout << str[i];
+    }
+
+    cout << endl;
+    return str;
+}
 int main()
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    setlocale(LC_ALL, "ru");
     char switch_on;
     do
     {
@@ -31,7 +96,66 @@ int main()
             break;
         }break;
 
+        case '1': {
+            char str1[256];
+            cout << "¬вед≥ть строку --> ";
+            cin.getline(str1, 256);
+            char str2[256];
+            cout << "¬вед≥ть строку --> ";
+            cin.getline(str2, 256);
+            cout << "str1 --> " << str1 << endl;
+            cout << "str2 --> " << str2 << endl;
+            cout << "str1 --> " << mystrlen(str1) << endl;
+            cout << "str2 --> " << mystrlen(str2) << endl;
+            if (mystrlen(str1) > mystrlen(str2)) {
+                cout << "str1" << " > " << "str2" << endl;
+            }
+            else if (mystrlen(str1) < mystrlen(str2)) {
+                cout << "str1" << " < " << "str2" << endl;
+            }
+            else if (mystrlen(str1) == mystrlen(str2)) {
+                cout << "str1" << " = " << "str2" << endl;
+            }
+            system("pause");
+        }break;
+        case '2': {
+            char str1[256];
+            cout << "¬вед≥ть строку --> ";
+            cin.getline(str1, 256);
+            cout << StringToNumber(str1) << endl;
+            system("pause");
+        }break;
+        case '3':
+        {
+            int number;
+            cout << "¬вед≥ть строку --> ";
+            cin >> number;
+            cout << NumberToString(number) << endl;
+            system("pause");
+        }break;
+        case '4':
+        {
+            char str1[256];
+            cout << "¬вед≥ть строку --> ";
+            cin.getline(str1, 256);
+            cout << Uppercase(str1) << endl;
 
+            system("pause");
+        }break;
+        case '5':
+        {
+            char str1[256];
+            cout << "¬вед≥ть строку --> ";
+            cin.getline(str1, 256);
+            cout << Lowercase(str1) << endl;
+
+            system("pause");
+        }break;
+        case '6': {
+            char str[] = {"Hello"};
+            cout << mystrrev(str) << endl;
+            system("pause");
+        }break;
         default:
             break;
         }
