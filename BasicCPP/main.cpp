a5b54d52949d0fede9fe5df3b4347314560d7cfc
Катя <@DESKTOP-VT3CEPG>
2023-01-22 18:00:37 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -34,36 +34,56 @@ int main()
         }break;
 
         case '1':
-        { fstream file;
-        fstream file2;
+        {
+            fstream file;
+            fstream file2;
+            char a[256] = "Hello";
+            char a1[256] = "World";
+            char a2[256] = "avocado";
+            char A = 65;
+            char b = 97;
+            int q = 0;
+            int w = 0;
 
-        file.open("file.txt", ios::out);
-        if (file.is_open())
-        {
-            file << " Hello " << endl;
-            file << " World " << endl;
-            file << " homeeeeee " << endl;
-            file << " hiiiiii " << endl;
-            file << " qqwerty " << endl;
-            file << " qawsedrf " << endl;
-            file << " my " << endl;
-            file << " rixoxxx" << endl;
-            file << " www " << endl;
-            file << " 12342 " << endl;
-            file.close();
-        }
-        else
-        {
-            cout << " Error. file not found!! " << endl;
-        }
-
-        ifstream input("file.txt");
-        string string;
-        ofstream output("file2.txt");
-        while (getline(input, string))
-        {
-            output << string << endl;
-        }
+            file.open("file.txt", ios::out);
+            if (file.is_open())
+            {
+                file << a << endl;
+                file << a1 << endl;
+                file << a2 << endl;
+                file.close();
+            }
+            else
+            {
+                cout << " Error. file not found!! " << endl;
+            }
+            for (uint32_t i = 0; i < 26; i++)
+            {
+                a[i] = a[256];
+                if (a[i] = A) {
+                    q++;
+                    ifstream input("file.txt");
+                    string string;
+                    ofstream output("file2.txt");
+                    while (getline(input, string))
+                    {
+                        output << string << endl;
+                    }
+                }
+                if (a[i] = b) {
+                    q++;
+                    ifstream input("file.txt");
+                    string string;
+                    ofstream output("file2.txt");
+                    while (getline(input, string))
+                    {
+                        output << string << endl;
+                    }
+                }
+                A++;
+                b++;
+            }
+            
             system("pause");
 
         }break;
