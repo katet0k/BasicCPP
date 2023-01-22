65a6c97cc6e460afd779e30085e5684b7ec0cbd0
Катя <@DESKTOP-VT3CEPG>
2023-01-22 18:33:13 +0200

    0

--- BasicCPP/main.cpp
+++ BasicCPP/main.cpp
@@ -1,42 +1,16 @@
 #include <iostream>
-#include <conio.h>
-#include <Windows.h>
+#define Main int main()
+#define begin {
+#define end }
+#define print(value) std::cout << value
+#define println(value) std::cout << value << std::endl;
+#define endLine cout << std::endl;
+#define sum(x, y) x+y
 using namespace std;
-
-int main()
-{
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, WORD(0 << 0 | 7));
-
-    setlocale(LC_ALL, "ru");
-    char switch_on;
-    do
-    {
-        system("cls");
-        cout << "    #=====================MENU====================#" << endl;
-        cout << "     |                                           | " << endl;
-        cout << "     |                                           | " << endl;
-        cout << "    #=============================================#" << endl;
-        cout << "     |   0 - Exit                                |" << endl;
-        cout << "    #=============================================#" << endl;
-
-        cout << "\n --> ";
-        switch_on = _getch();
-        system("cls");
-        cout << endl;
-
-        switch (switch_on)
-        {
-        case '0': {
-            break;
-        }break;
-
-
-        default:
-            break;
-        }
-    
-    } while (switch_on != '0');
-
-	return 0;
-}
+Main
+begin
+println("hello");
+println(sum(2, 3));
+endLine;
+return 0;
+end
